4b71923d24c7cdf030f37384355717d2a2e1bf13
Mare Raul <marerauldan@yahoo.ro>
2018-12-12 11:49:27 +0200

    l-am stricat

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Random rng = new Random();
+            int r = rng.Next(1, 100);
+            int nr = 0;
+            bool corect = false;
+
+            Console.WriteLine("Am generat un numar intre 1 si 100.");
+
+            while (!corect)
+            {
+ 
+                Console.Write("Ghiceste-l: ");
+                string input = Console.ReadLine();
+
+                if (!int.TryParse(input, out nr))
+                {
+                    Console.WriteLine("Acesta nu este un numar.");
+                    continue;
+                }
+
+                if (nr < r)
+                {
+                    Console.WriteLine("Numarul este prea mic ");
+                }
+                else if (nr > r)
+                {
+                    Console.WriteLine("Numarul este prea mare ");
+                }
+                else
+                {
+                    corect = true;
+                    Console.WriteLine("Numarul este corect!");
+                }
+
+            }
+        }
+    }
+}
